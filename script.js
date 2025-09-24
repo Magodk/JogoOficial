d0987884ec526fc07864b6b4f6ddee5f8481339a
Magodk <magnoscripting@gmail.com>
2025-09-24 20:06:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, on
 import { getFirestore, doc, setDoc, getDoc, collection, getDocs, updateDoc, deleteDoc, runTransaction } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3jZXUDV0xJhuyKDY8zKe_Ym26R-W6E4o",
-  authDomain: "meu-jogo-v2.firebaseapp.com",
-  projectId: "meu-jogo-v2",
-  storageBucket: "meu-jogo-v2.firebasestorage.app",
-  messagingSenderId: "573833700374",
-  appId: "1:573833700374:web:460fdee5567a9565fd4137"
-};
+    apiKey: "AIzaSyAoHz8j6blx7nQTVxUyOOQ_Mg4MMF2ThGg",
+    authDomain: "meu-jogo-online-880e0.firebaseapp.com",
+    projectId: "meu-jogo-online-880e0",
+    storageBucket: "meu-jogo-online-880e0.firebasestorage.app",
+    messagingSenderId: "604190129868",
+    appId: "1:604190129868:web:4c45c49f5bd1b3c0718c69",
+    measurementId: "G-QM9QYRFX8T"
 };
 
 const app = initializeApp(firebaseConfig);
